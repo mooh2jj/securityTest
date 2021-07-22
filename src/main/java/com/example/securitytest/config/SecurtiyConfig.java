ac7e6919559865d019843953058f7e97e5291d0c
도성곤 <sg.do@unionplace.co.kr>
2021-07-22 16:56:56 +0900

    security test 버전 네번째 구현 jwt 구현 성공.-> addFilterBefore 변경, 성공

--- src/main/java/com/example/securitytest/config/SecurtiyConfig.java
+++ src/main/java/com/example/securitytest/config/SecurtiyConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -36,24 +38,28 @@ public class SecurtiyConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				// 토큰 기반 인증이므로 세션도 사용 x
 			.and()
-			.addFilter(new JwtAuthenticationFilter(authenticationManager()))
-			.addFilter(new JwtAuthorizationFilter(authenticationManager(), userRepository))
+//			.addFilter(new JwtAuthenticationFilter(authenticationManager()))
+//			.addFilter(new JwtAuthorizationFilter(authenticationManager(), userRepository))
 				.authorizeRequests()
-			.antMatchers("/user/**").authenticated()
-			.antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
-			.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
-			.anyRequest().permitAll()
+				.antMatchers("/user/**").authenticated()
+				.antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+				.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+				.anyRequest().permitAll()
 			.and()
-			.formLogin()
-			.loginPage("/loginForm")
-			.loginProcessingUrl("/login")
-			.defaultSuccessUrl("/")
-		.and()
-			.oauth2Login()
-			.loginPage("/loginForm")
-			.userInfoEndpoint()
-			.userService(principalOauthUserService)
+				.addFilterBefore(new JwtAuthenticationFilter(authenticationManager()), UsernamePasswordAuthenticationFilter.class)
+				.addFilterBefore(new JwtAuthorizationFilter(authenticationManager(), userRepository), BasicAuthenticationFilter.class)
+				.formLogin()
+				.loginPage("/loginForm")
+				.loginProcessingUrl("/login")
+				.defaultSuccessUrl("/")
+			.and()
+				.oauth2Login()
+				.loginPage("/loginForm")
+				.userInfoEndpoint()
+				.userService(principalOauthUserService)
+
 		;
 
 	}
